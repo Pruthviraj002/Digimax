1c43603bcd838b0747fe561db85f71e1fcea2d88
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-07-26 13:33:02 +0530

    simple-website

---
+++ app.js
@@ -0,0 +1,270 @@
+// gsap.to("#hero", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 100%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero1", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero1",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 100%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero2 img", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero2 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero2 #para", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero2 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 2,
+//         pin: true
+//     }
+// })
+
+
+
+
+// gsap.to("#hero3 #img-info", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero3 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero3 #top-para", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero3 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero3 #bottom-para", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero3 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+// gsap.to("#hero5 .para ", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero5 ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero5 .cards", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero5 .cards",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero5 #card2", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero5  #card2",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero6 .info-center-top", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero6  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+
+// gsap.to("#hero6 .image", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero6  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero7 .para", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero7 .para ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero7 #card1", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero7 #card1",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+
+// gsap.to("#hero7 #card2", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero7 #card2  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+// gsap.to("#hero7 #card3", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero7 #card3  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero7 #card4", {
+//     transform: "translateX(150%)",
+//     scrollTrigger: {
+//         trigger: "#hero7 #card4  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+// gsap.to("#hero8 .para", {
+//     transform: "translateX(-150%)",
+//     scrollTrigger: {
+//         trigger: "#hero8 .para  ",
+//         scroller: "body",
+//         // markers: true,
+//         start: "top 0%",
+//         end: "top 150%",
+//         scrub: 5,
+//         pin: true
+//     }
+// })
+
+
+
+
+
+
+
+
+
